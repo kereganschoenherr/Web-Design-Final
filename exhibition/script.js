d1fd01f3bd812f910a79efb3756de4904cf0bfe8
Keregan Schoenherr <34699991+kereganschoenherr@users.noreply.github.com>
2017-12-19 23:43:49 -0500

    Update script.js

--- exhibition/script.js
+++ exhibition/script.js
@@ -1,76 +1,28 @@
 
 
-	var creatureSlides = document.getElementsByClassName("leftImage");
-	var humanSlides = document.getElementsByClassName('rightImage');
-	//var bg = document.getElementsByClassName("bg");
-	//var bg2 = document.getElementsByClassName("bg2");
-
-	var colors = ['blue','green','orange','yellow'];
-	var colors2 = ['aliceblue','lightgreen','pink','antiquewhite'];
-
+var creatureSlides = document.getElementsByClassName("leftImage");
+var humanSlides = document.getElementsByClassName('rightImage');
 	
-function changeImage(){
-	document.getElementById('leftImage').src='../images/faces_bumper.png';
-	var x = document.getElementById("rightImage");
-	x.src = "../images/annie_bumper.png";
-}
+
 function slideShow(){
 	
 	for(i = 0; i < humanSlides.length;i++){
-		/*
-		bg[i].style.display = "none";
-		bg2[i].style.display = "none";
-		*/
+		
 		creatureSlides[i].style.display = "none";
 		humanSlides[i].style.display = "none";
 
 	}
 	creatureSlides[index].style.display = "block";
 	humanSlides[index].style.display = "block";
-	/*
-	bg[index].style.backgroundColor = colors[index];
-	bg[index].style.display = "block";
-	bg2[index].style.backgroundColor = colors2[index];
-	bg2[index].style.display = "block";
+	
 
-*/
+
 	
 	index++;
 	if(index == humanSlides.length){index = 0}
 	setTimeout(slideShow,6000);	
 }
-/*
-function testSlide1(){
 
-	
-	creatureSlides[index].style.display = "block";
-	humanSlides[index].style.display = "block";
-	bg[index].style.backgroundColor = colors[index];
-	bg[index].style.display = "block";
-
-
-	
-	index++;
-	if(index == humanSlides.length){index = 0}
-	setTimeout(testSlide1,6000);	
-
-}
-function testSlide2(){
-
-	
-	creatureSlides[index].style.display = "block";
-	humanSlides[index].style.display = "block";
-	bg[index].style.backgroundColor = colors[index];
-	bg[index].style.display = "block";
-
-
-	
-	index++;
-	if(index == humanSlides.length){index = 0}
-	
-
-}
-*/
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
